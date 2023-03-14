8f44f7e18e41936e3926229463f799acf1a9c35a
bilbao_iban <bilbao_iban@eitb.eus>
2023-03-14 15:37:00 +0100

    zatika

--- src/js/ItzultzaileNeuronala.js
+++ src/js/ItzultzaileNeuronala.js
@@ -16,30 +16,21 @@ class ItzultzaileNeuronala
                 xml_prepare = Xml.prepare(text)
                 text        = xml_prepare.text
             }
-            fetch(self.getUrl(),
-            {
-                method: 'POST',
-                body:   JSON.stringify(
-                {
-                    mkey:     "8d9016025eb0a44215c7f69c2e10861d",
-                    model:    self.getModel(),
-                    text:     text,
-                })
-            })
-            .then((response) => response.json())            
-            .then(function(r)
+            let parts = self.split(text)
+            self.serie(parts, 0, '')
+            .then(function(response)
             {
                 switch(mode)
                 {
                     case 'xml':
-                        r.message = Xml.replace(xml_prepare, r.message)
+                        response = Xml.replace(xml_prepare, response)
                         break;
                 }
-                resolve(r.message)
+                resolve(response)
             })
             .catch(function(e)
             {
-                console.error(e)
+                console.error('get', e)
                 reject(e)
             })
         })
@@ -60,6 +51,85 @@ class ItzultzaileNeuronala
     {
         const type = this.getType()
         return `generic_`+type
+    }
+
+    static split(data)
+    {
+        const limit  = 10000
+        let   chunks = data.split(/\n/)
+        const cl     = chunks.length
+        
+        let   parts  = ['']
+        let   j      = 0
+        
+        for(let i=0; i<cl; i++)
+        {
+            chunks[i] = chunks[i]+"\n"
+            if(chunks[i].length+parts[j].length>limit)
+            {
+                    j++
+                    parts[j] = ''
+            }
+            parts[j] += chunks[i]
+        }
+        parts[j] = parts[j].replace(/\n$/, '')
+        return parts
+    }
+
+    static request(text)
+    {
+        let self = this
+        return new Promise(function(resolve, reject)
+        {
+            fetch(self.getUrl(),
+            {
+                method: 'POST',
+                body:   JSON.stringify(
+                {
+                    mkey:     "8d9016025eb0a44215c7f69c2e10861d",
+                    model:    self.getModel(),
+                    text:     text,
+                })
+            })
+            .then((response) => response.json())            
+            .then(function(r)
+            {
+                resolve(r.message)
+            })
+            .catch(function(e)
+            {
+                console.error('request', e)
+                reject(e)
+            })
+        })
+    }
+
+    static serie(parts, i, result, timeout)
+    {
+        let self = this
+        timeout = (i===0)?0:0.5 * 1000;
+
+        return new Promise(function(resolve, reject)
+        {
+            window.setTimeout(function()
+            {
+                self.request(parts[i])
+                .then(function(response)
+                {
+                    result += response
+                    i++
+                    if(typeof parts[i]!=='undefined')
+                        resolve(self.serie(parts, i, result, timeout))
+                    else
+                        resolve(result)
+                })
+                .catch(function(e)
+                {
+                    console.error('serie', e)
+                    reject(e)
+                })
+            }, timeout)
+        })
     }
 }
 
